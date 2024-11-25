c74596b8e640fb675c0078f06e0dc9174683a71f
that1flame <flameking373@gmail.com>
2024-11-24 21:52:23 -0500

    fix

--- js/layers/classn1.js
+++ js/layers/classn1.js
@@ -15,7 +15,6 @@ addLayer("n1", {
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
-        if (hasUpgrade('n1', 13)) mult = mult.times(upgradeEffect('n1', 13))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
